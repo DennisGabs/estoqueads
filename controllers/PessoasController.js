b2b4a37984b4c541daf4b9b36bc0fa54ca238db2
DENNIS GABRIEL SILVA ALVES <20232ADS.CNT0013@ifma.edu>
2025-02-05 20:02:50 -0300

    add Pessoas

--- controllers/PessoasController.js
+++ controllers/PessoasController.js
@@ -1,9 +1,39 @@
 import Pessoa from "../models/Pessoa.js";
+import Usuario from "../models/Usuario.js";
 
 class PessoasController {
     index = async (req, res) => {
         let pessoas = await Pessoa.findAll()
         res.render('pessoas/index', { pessoas: pessoas })
+    }
+
+    cadastrar = (req, res) => {
+        res.render('pessoas/cadastrar')
+    }
+
+    salvar = (req, res) => {
+        const pessoa = {
+            cpf: req.body.cpf,
+            nome: req.body.nome,
+            telefone: req.body.telefone,
+            email: req.body.email
+        }
+
+        Pessoa.create(pessoa).then(() => {    
+            res.redirect('/pessoas')
+        })
+    }
+
+    perfil = async (req, res) => {
+        let id = req.params.id
+
+        const pessoa = await Pessoa.findByPk(id)
+        const usuarios = await Usuario.findAll({
+            where: {
+                pessoa_id: id
+            }
+        })
+        res.render('pessoas/perfil', {pessoa: pessoa, usuarios: usuarios})
     }
 }
 
--- routes/pessoa.js
+++ routes/pessoa.js
@@ -2,6 +2,12 @@ import express from "express"
 import PessoasController from "../controllers/PessoasController.js"
 const router = express.Router()
 
-router.get('/index', PessoasController.index)
+router.get('/', PessoasController.index)
+
+router.get('/cadastrar', PessoasController.cadastrar)
+
+router.post('/salvar', PessoasController.salvar)
+
+router.get('/perfil', PessoasController.perfil)
 
 export default router
